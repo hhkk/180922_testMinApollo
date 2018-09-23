0652137c1fc9e055b670c404f3f460fe8bfc95c5
hhkk <hank.khang@gmail.com>
2018-09-23 07:05:34 -0400

    sdf

---
+++ server/index.js
@@ -0,0 +1,75 @@
+// from https://github.com/prisma/graphql-yoga
+//import { GraphQLServer } from 'graphql-yoga'
+// ... or using `require()`
+const { GraphQLServer } = require('graphql-yoga')
+
+
+// from https://mongoosejs.com/docs/index.html
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test180902mern', { useNewUrlParser: true }); // hbkhbk db server mongo port config
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log ('connected to the db hbk! ')
+});
+
+const Todo = mongoose.model('Todo', {
+    text: String,
+    complete: Boolean
+
+})
+
+
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+    todos: [Todo]
+  }
+  
+  type Todo {
+    id: ID!
+    text: String!
+    complete: Boolean!
+  }
+  
+  type Mutation {
+    createTodo(text: String!): Todo
+    updateTodo (id: ID!, complete: Boolean!): Boolean
+    removeTodo (id: ID!): Boolean
+  }
+`
+
+const resolvers = {
+    Query: {
+        hello: (_, {name}) => `Hello ${name || 'World'}`,
+        todos: () => Todo.find()
+    },
+    Mutation: {
+        createTodo: async (_, {text}) => {
+            const todo = new Todo({text, complete: false});
+            await todo.save();
+            return todo;
+        },
+        updateTodo: async (_, {id, complete}) => {
+
+            // from https://www.apollographql.com/docs/graphql-tools/generate-schema.html
+            // const post = find(posts, { id: postId });
+            // if (!post) {
+            //     throw new Error(`Couldn't find post with id ${postId}`);
+            // }
+
+            await Todo.findByIdAndUpdate(id, {complete});
+            return true;
+        },
+        removeTodo: async (_, {id}) => {
+            await Todo.findByIdAndRemove(id);
+            return true;
+        }
+    }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
